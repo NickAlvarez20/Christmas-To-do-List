4b6c150d828e44f5c5c4df068e76235493f18141
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2025-12-20 23:25:42 -0700

    add: Add useState and create state variable called todos
    -UseState is a hook, lets you add state(data that can change over time) to components.
    -Without useState it would be similar to a static html tag with a predefined value that doesn't change.
    -Create variable using array destructing and React conventions.
    -Name the state whatever you want: todos, count, user, items...
    -Name the setter function set+the state name: setTodos, setCount, setUser.
    -The setter function updates the state of the app and triggers a re-render when updated.
    -The setter can be called anywhere inside the component(e.g., in event handlers).
    -To let child components update the state, we pass down the setter (or a callback that uses it) as a prop.
    
    Learn about useState and React useState naming conventions / i.e., [name, setterName]

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div>
       <h1>My Todo App</h1>
-      <p>Hello from React!</p>
+      <p>You have {todos.length} todos</p>
     </div>
   );
 }
